53bf34ac8b45b2ba32f6044837e49f2e42a63993
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2022-06-07 12:01:49 -0400

    edit checkSite, add checkSites, add checkResults function. Currently working on checkResults but might change stuff in checkSite

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   let companies = []
+  const start = Date.now()
+
     const browser = await puppeteer.launch({ headless: false }); 
   const page = await browser.newPage();
   await page.setRequestInterception(true)
@@ -43,18 +45,22 @@ const puppeteer = require('puppeteer');
   // function to see if it has a website, if so, then says website, if not, logs 'add a website'
 
   async function checkSite(url){
-    const res = await page.goto(url);
-    const text = await res.text()
-    const dom = await new JSDOM(text)
+    // const res = await page.goto(url);
+    // const text = await res.text()
+    // const dom = await new JSDOM(text)
+
+    await page.waitForSelector('.fontHeadlineLarge > span')
+    
     // THIS GRABS THE SITE BAYBEEEE 
     try{
       const site = await page.$eval('[aria-label^="Website:"] > div div + div > div', (el) => el.innerHTML)
-      console.log(site)
+      console.log(site)      
       
     }
     catch{
+      
       console.log('add a website')
-      const companyName = await page.$eval('.fontHeadlineLarge > span', (el) => el.innerHTML)
+      companyName = await page.$eval('.fontHeadlineLarge > span', (el) => el.innerHTML)
       await checkPhone()
       // try{
       //   checkPhone()
@@ -81,18 +87,46 @@ const puppeteer = require('puppeteer');
       const phone = await page.$eval('[aria-label^="Phone:"] > div div + div > div', (el) => el.innerHTML)
       console.log(phone)
       companies.push({
-        compName: companyName,
+        companyName: companyName,
         phone: phone
       })
       console.log(companies)
     }
     catch{
       console.log('no number listed')
+      companies.push({
+        companyName: companyName,
+        phone: `No number listed`
+      })
+      console.log(companies)
     }
   }
 
-  await checkSite('https://goo.gl/maps/xbJzXuMEbcuDRwZJ6')
+  async function checkSites(urls){
+    for(let i = 0; i < urls.length; i++){
+      await checkSite(urls[i])
+    }
+  }
+
+  async function clickResult(url){
+    const res = await page.goto(url);
+    const text = await res.text()
+    const dom = await new JSDOM(text)
+    for(let i = 3; i < 43; i += 2){
+      await page.click(`#pane + div > div > div > div> div:nth-child(2) > div > div > div > div > div > div > div:nth-child(${i}) > div > a`)
+      await checkSite()
+      // await page.click("#pan + div  > div > div > div > div > div > div > div > div > div > div > div > div:nth-child(3) > span > button")
+    }
+    
+    
+  }
+
+  // await checkSites([`https://www.google.com/maps/place/Talia's+Tuscan+Table/@26.3728166,-80.1523849,13z/data=!4m9!1m2!2m1!1srestaurants!3m5!1s0x88d8e1c3fe6ad2bb:0x9ce3aa4bad89137e!8m2!3d26.386941!4d-80.080988!15sCgtyZXN0YXVyYW50c1oNIgtyZXN0YXVyYW50c5IBEml0YWxpYW5fcmVzdGF1cmFudA`])
+  await clickResult('https://www.google.com/maps/search/Restaurants/@26.3728081,-80.1523849,13z/data=!3m1!4b1')
   await browser.close();
+  const stop = Date.now()
+  console.log(`Time taken to execute: ${(stop - start)/1000} seconds`)
+  
   
 
 }

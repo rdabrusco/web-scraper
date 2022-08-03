09c8173a6407124ec861af68d090b4be1cd49050
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2022-08-02 20:20:37 -0400

    remove some functions that didnt work correctly

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const { JSDOM } = jsdom
 
 
 const puppeteer = require('puppeteer');
+const { scrollPageToBottom } = require('puppeteer-autoscroll-down');
 
 (async () => {
   let companies = []
   const start = Date.now()
 
-    const browser = await puppeteer.launch({ headless: true }); 
+    const browser = await puppeteer.launch({ headless: false }); 
   const page = await browser.newPage();
   await page.setRequestInterception(true)
   page.on('request',(req) =>{
@@ -64,6 +65,7 @@ const puppeteer = require('puppeteer');
       console.log(companies)
     }
     catch{
+      const phone = await page.$eval('[aria-label^="Phone:"] > div div + div > div', (el) => el.innerHTML)
       console.log('no number listed')
       companies.push({
         companyName: companyName,
@@ -81,6 +83,10 @@ const puppeteer = require('puppeteer');
 
   async function clickResult(url){
     const res = await page.goto(url);
+    const lastPosition = await scrollPageToBottom(page, {
+      size: 500,
+      delay: 250
+    })
     const text = await res.text()
     const dom = await new JSDOM(text)
 
@@ -89,33 +95,69 @@ const puppeteer = require('puppeteer');
     }
 
     // try this out tomorrow
-    async function autoScroll(page){
-      await page.evaluate(async () => {
-          await new Promise((resolve, reject) => {
-              var totalHeight = 0;
-              var distance = 300;
-              var timer = setInterval(() => {
-                  var scrollHeight = document.body.scrollHeight;
-                  window.scrollBy(0, distance);
-                  totalHeight += distance;
+  //   async function autoScroll(){
+  //     await page.evaluate(async () => {
+  //         await new Promise((resolve, reject) => {
+  //             var totalHeight = 0;
+  //             var distance = 300;
+  //             var timer = setInterval(() => {
+  //                 const element = page.$('[role="main"]').scrollHeight
+  //                 console.log(element)
+  //                 var scrollHeight = element.scrollHeight
+  //                 element.scrollBy(0, distance);
+  //                 totalHeight += distance;
   
-                  if(totalHeight >= scrollHeight - window.innerHeight){
-                      clearInterval(timer);
-                      resolve();
-                  }
-              }, 100);
-          });
-      });
-  }
+  //                 if(totalHeight >= scrollHeight - window.innerHeight){
+  //                     clearInterval(timer);
+  //                     resolve();
+  //                 }
+  //             }, 100);
+  //         });
+  //     });
+  // }
 
-    for(var i = 3; i < 15; i += 2){
+  function extractItems() {
+    const extractedElements = document.querySelectorAll('.MyEned span.wiI7pd');
+    const items = [];
+    for (let element of extractedElements) {
+      items.push(element.innerText);
+    }
+    return items;
+  }
+  
+  
+  async function scrapeItems(
+    page,
+    extractItems,
+    itemCount,
+    scrollDelay = 2000,
+  ) {
+    let items = [];
+    try {
+      let previousHeight;
+      while (items.length < itemCount) {
+        items = await page.evaluate(extractItems);
+        previousHeight = page.evaluate('document.querySelector("div.m6QErb.DxyBCb").scrollHeight');
+        await page.evaluate(`document.querySelector("div.m6QErb.DxyBCb").scrollTo(0, ${previousHeight[0]})`);
+        await page.waitForFunction(`document.querySelector("div.m6QErb.DxyBCb").scrollHeight > ${previousHeight[0]}`);
+        await page.waitForTimeout(scrollDelay);
+      }
+    } catch(e) { }
+    return items;
+  }
+  // await autoScroll()
+
+    for(var i = 3; i < 41; i += 2){
       console.log(i)
       try{
         await page.waitForSelector(`#pane + div > div > div > div> div:nth-child(2) > div > div > div > div > div > div > div:nth-child(${i}) > div > a`)
       }
       catch{
       await scrollDown()
-      // await scrollDown()
+      // await lastPosition()
+      await scrollDown()
+      await scrollDown()
+
      
 
 
@@ -125,12 +167,14 @@ const puppeteer = require('puppeteer');
       // await scrollDown()
 
       }
+      await scrollDown()
       
       await page.click(`#pane + div > div > div > div> div:nth-child(2) > div > div > div > div > div > div > div:nth-child(${i}) > div > a`)
       await checkSite()
       await page.click(`[aria-label^="Back"]`)
+      
       // await scrollDown()
-      await scrollDown()
+      // await scrollDown()
       
 
       
@@ -143,6 +187,7 @@ const puppeteer = require('puppeteer');
   
 
   // await checkSites([`https://www.google.com/maps/place/Talia's+Tuscan+Table/@26.3728166,-80.1523849,13z/data=!4m9!1m2!2m1!1srestaurants!3m5!1s0x88d8e1c3fe6ad2bb:0x9ce3aa4bad89137e!8m2!3d26.386941!4d-80.080988!15sCgtyZXN0YXVyYW50c1oNIgtyZXN0YXVyYW50c5IBEml0YWxpYW5fcmVzdGF1cmFudA`])
+  
   await clickResult('https://www.google.com/maps/search/Restaurants/@26.3728081,-80.1523849,13z/data=!3m1!4b1')
   await browser.close();
   const stop = Date.now()
